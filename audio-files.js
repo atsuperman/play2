0e2f731350add1f2827a0e0b7458c4b5af78dd0f
atsuperman <smstarvinoth@gmail.com>
2024-12-18 21:36:39 +0530

    Create audio-files.js

---
+++ audio-files.js
@@ -0,0 +1,5 @@
+// List of audio file URLs
+const audioFiles = [
+  "https://github.com/atsuperman/play2/raw/refs/heads/main/Achacho.mp3",
+  "https://github.com/atsuperman/play2/raw/refs/heads/main/DIVINE%20-%20Triple%20OG%20_%20Prod.%20by%20Phenom%20_%20Official%20Music%20Video(MP3_320K).mp3"
+];
