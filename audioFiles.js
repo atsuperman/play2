6edbbd2f6858f4d3eec35ff75100ca4514b9d21a
atsuperman <smstarvinoth@gmail.com>
2025-01-21 21:05:20 +0530

    Update audioFiles.js

--- audioFiles.js
+++ audioFiles.js
@@ -1,9 +1,9 @@
 // Define repository URLs
-const repository1 = "https://repository1.example.com/";
-const repository2 = "https://repository2.example.com/";
+const repository1 = "https://github.com/atsuperman/play2/raw/refs/heads/main/";
+const repository2 = "https://github.com/atsuperman/play2/raw/refs/heads/main/";
 
 // Define file names for each repository
-const repo1Songs = ["example.mp3", "example2.mp3"];
+const repo1Songs = ["Achacho.mp3", "example2.mp3"];
 const repo2Songs = ["song1.mp3", "song2.mp3"];
 
 // Combine all songs with their full paths
